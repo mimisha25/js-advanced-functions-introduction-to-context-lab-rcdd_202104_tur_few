4f6bfdadd4803fc5fd5052d409ef71ca5d860a38
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-14 11:46:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,52 +8,39 @@ let createEmployeeRecord = function(arr){
          timeInEvents: [],
          timeOutEvents: []
      }
-
  }
 
  let createEmployeeRecords = function(arr){
      return arr.map(record => createEmployeeRecord(record))
  }
 
-
-
-
  let createTimeInEvent = function(employee, dateStamp){
      let [date, hour] = dateStamp.split(' ')
-
      employee.timeInEvents.push({
          type: "TimeIn",
          hour: parseInt(hour, 10),
          date,
      })
-
      return employee
  }
 
-
-
  let createTimeOutEvent = function(employee, dateStamp){
-     let [date, hour] = dateStamp.split(' ')
-
+ let [date, hour] = dateStamp.split(' ')
      employee.timeOutEvents.push({
          type: "TimeOut",
          hour: parseInt(hour, 10),
          date,
      })
-
      return employee
  }
-
 
  let hoursWorkedOnDate = function(employee, soughtDate){
      let inEvent = employee.timeInEvents.find(function(e){
          return e.date === soughtDate
      })
-
      let outEvent = employee.timeOutEvents.find(function(e){
          return e.date === soughtDate
      })
-
      return (outEvent.hour - inEvent.hour) / 100
  }
 
@@ -67,11 +54,9 @@ let createEmployeeRecord = function(arr){
      let eligibleDates = employee.timeInEvents.map(function(e){
          return e.date
      })
-
      let payable = eligibleDates.reduce(function(memo, d){
          return memo + wagesEarnedOnDate(employee, d)
      }, 0)
-
      return payable
  }
 
